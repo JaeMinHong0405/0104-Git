640bbc281ce1f94532e19c772984cac62c6433e1
hjm9957 <hjm9957@gmail.com>
2024-01-05 18:05:08 +0900

    Edited Final

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ from print2 import print2
 if __name__ == "__main__":
   print1()
   print2()
+
+
+# git init
+# git status
+# git add .
+# git log
+# git commit -m "Initial Commit"
+# git config --global user.name
+# git config --global user.email
+# git remote add origin 
+# git remote
+# git remote -v
+# git config --global credential.username
+# git log --all --graph
+# git push origin master
+# git add
+# git commit -m "Edited Version"
+# git push origin master
+# git push origin master -set-upstream
